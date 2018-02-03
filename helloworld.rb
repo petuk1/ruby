0c9c3b520ff2b3b37fcf1639aeec6e8869f4dc04
petuk1 <bugajski.tomek@gmail.com>
2018-02-03 16:09:44 +0100

    intial version

---
+++ helloworld.rb
@@ -0,0 +1,23 @@
+=begin
+print("Enter your first name:")
+firstName = gets.chomp
+print("Enter your last name:")
+lastName = gets.chomp
+print("Enter your age:")
+age = gets.chomp
+
+
+newage = age.to_i
+
+if newage > 30
+	puts("Your name is #{firstName} #{lastName} and you are #{age} years old")
+else
+	puts("Your name is #{firstName} #{lastName} and you are really young")
+end
+=end
+
+print("Enter link: ")
+link = gets.chomp
+print("Searchterm: ")
+term = gets.chomp
+system "curl #{link} | html2text | grep #{term}"
